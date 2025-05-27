721b9480bda5b055025644707be7cf826639a832
Dasun-Sandaru <105912727+Dasun-Sandaru@users.noreply.github.com>
2025-05-27 15:22:46 +0530

    add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Nav from './assets/nav'
 import './App.css'
 
 function App() {
@@ -16,7 +17,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Nav />
+      <h1>Vite + React App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
